5ff94fe3d679cbff64cb4c210411b609f7bfba7f
Martin Suehowicz <martin_2110@yahoo.com>
2015-08-08 19:23:21 -0700

    .

--- game.rb
+++ game.rb
@@ -1,23 +1,70 @@
 class Game
   require_relative 'player'
+  require_relative 'die'
+  require_relative 'game_turn'
+  require_relative 'treasure_trove'
+
   attr_reader :title
+
   def initialize(title)
-    @title = title
+    @title   = title
     @players = Array.new
   end
+
   def add_player(player)
     @players << player
   end
-  def play
-    puts "There are #{@players.size} players in #{@title}: "
-    @players.each do |player|
-      puts player
+  def print_name_and_health(player)
+    puts "#{player.name} (#{player.health})"
+  end
+  def print_stats
+    strong_players, wimpy_players   =  @players.partition  {|p1| p1.strong? }
+    puts "\n#{@title} Statistics:"
+
+    puts "\n#{strong_players.size} strong players:"
+    strong_players.each do |player|
+      print_name_and_health(player)
     end
-    @players.each do |player|
-      player.blam
-      player.w00t
-      player.w00t
-      puts player
+
+    puts "\n#{wimpy_players.size} wimpy players:"
+    wimpy_players.each do |player|
+      print_name_and_health(player)
+    end
+
+    puts "\n#{@title} High Scores:"
+    @players.sort.each do |player|
+      formatted_name = player.name.ljust(20, '.')
+      puts "#{formatted_name} #{player.score}"
+    end
+
+    @players.sort.each do |player|
+      puts "\n#{player.name}'s point totals:"
+      puts "#{player.points} grand total points"
+    end
+
+  end
+  def total_points
+    @players.map { |p| p.points }.reduce(0, :+)
+    @players.reduce(0) { |sum, player| sum + player.points }
+  end
+  def play(round)
+    1.upto(round) do
+      puts "\nRound #{round}:"
+      puts "There are #{@players.size} players in : "
+      @players.each do |player|
+        puts player
+      end
+
+      @players.each do |player|
+        GameTurn.take_turn(player)
+        puts player
+      end
+    end
+    treasures = TreasureTrove::TREASURES
+    puts "There are #{treasures.length} treasures to be found:"
+    puts "\nThere are #{treasures.size} treasures to be found:"
+    treasures.each do |treasure|
+      puts "A #{treasure.name} is worth #{treasure.points} points"
     end
   end
 end
--- player.rb
+++ player.rb
@@ -5,6 +5,10 @@ class Player
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
+    @found_treasures = Hash.new(0)
+  end
+  def <=>(old)
+   old.score <=> self.score
   end
   def say_hello
     "I'm #{@name} with a health of #{@health}"
@@ -18,18 +22,33 @@ class Player
     puts "#{@name} got w00ted!"
   end
   def to_s
-    "I'm #{@name} with a health of #{@health} and a score of #{score}."
+    "I'm #{@name} with health = #{@health}, points = #{points}, and score = #{score}."
   end
   def score
-    @name.length + @health
+    @health + self.points
+  end
+  def strong?
+    @health > 100
+  end
+  def found_treasure(treasure)
+    @found_treasures[treasure.name] += treasure.points
+    puts "#{@name} found  #{treasure.name} worth #{treasure.points} points"
+    puts "#{@name}'s treasures: #{@found_treasures}"
+  end
+  def points
+    @found_treasures.values.reduce(0, :+)
   end
 end
+
+
 if __FILE__ == $0
-  player = Player.new("moe")
+  player = Player.new("moe", 150)
   puts player.name
   puts player.health
   player.w00t
   puts player.health
   player.blam
   puts player.health
+  puts player.strong?
+  puts player.wimpy?
 end
